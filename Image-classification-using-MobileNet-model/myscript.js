f2db5805f89e51877d6e61df508e49b1a35f5223
Anh Dang Trung <dangtrunganh@gmail.com>
2021-07-10 14:46:06 +0700

    Image Classification program for Beginners

--- Image-classification-using-MobileNet-model/myscript.js
+++ Image-classification-using-MobileNet-model/myscript.js
@@ -1,9 +1,6 @@
 const image = document.getElementById('image');
-
 const result = document.getElementById('result');
-
 const probability = document.getElementById('probability');
-
 document.getElementById('getval').addEventListener('change', readURL, true);
 
 function readURL() {
@@ -12,24 +9,18 @@ function readURL() {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            image
-                .src = e.target.result;
+            image.src = e.target.result;
+            ml5.imageClassifier('MobileNet')
+                .then(classifier => classifier.classify(image)
+                    .then(results => {
+                        result.innerText = results[0].label;
+                        probability.innerText = results[0].confidence.toFixed(4);
+
+                    })
+                )
+
         };
 
         reader.readAsDataURL(file);
     }
 }
-
-ml5.imageClassifier('MobileNet')
-
-    .then(classifier => classifier.classify(image))
-
-    .then(results => {
-        
-        result.innerText = results[0].label;
-        probability.innerText = results[0].confidence.toFixed(4);
-
-    });
-
-
-
